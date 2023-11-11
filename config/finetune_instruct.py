88d14b5577aa3e709774610efb225e676faa38b1
daniel <dxdtran@outlook.com>
2023-11-11 09:13:55 -0800

    instruction fine tuning

--- config/finetune_instruct.py
+++ config/finetune_instruct.py
@@ -3,12 +3,12 @@ import time
 out_dir = 'out-instruct'
 eval_interval = 5
 eval_iters = 40
-wandb_log = False # feel free to turn on
+wandb_log = False  # feel free to turn on
 wandb_project = 'instruct'
 wandb_run_name = 'ft-' + str(time.time())
 
 dataset = 'instruct'
-init_from = 'gpt2-xl' # this is the largest GPT-2 model
+init_from = 'gpt2'  # this is the largest GPT-2 model
 
 # only save checkpoints if the validation loss improves
 always_save_checkpoint = False
